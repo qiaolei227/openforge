a391fe2bf2401bafcae1fcbeedc7b8c9dc335b73
david.qiao <286887534@qq.com>
2026-05-18 02:32:45 +0800

    feat(server): wire WorkflowEngineService + WorkflowLockHelper in module + import NotificationModule (P2.3)

--- apps/server/src/workflow/workflow.module.ts
+++ apps/server/src/workflow/workflow.module.ts
@@ -1,23 +1,30 @@
 import { Module } from '@nestjs/common';
+import { NotificationModule } from '../notification/notification.module';
 import { WorkflowService } from './workflow.service';
 import { WorkflowVersionService } from './workflow-version.service';
 import { WorkflowConditionMatcher } from './workflow-condition-matcher.service';
 import { AssigneeResolverService } from './assignee-resolver.service';
+import { WorkflowLockHelper } from './workflow-lock.helper';
+import { WorkflowEngineService } from './workflow-engine.service';
 import { WorkflowController } from './workflow.controller';
 
 @Module({
+  imports: [NotificationModule],
   controllers: [WorkflowController],
   providers: [
     WorkflowService,
     WorkflowVersionService,
     WorkflowConditionMatcher,
     AssigneeResolverService,
+    WorkflowLockHelper,
+    WorkflowEngineService,
   ],
   exports: [
     WorkflowService,
     WorkflowVersionService,
     WorkflowConditionMatcher,
     AssigneeResolverService,
+    WorkflowEngineService,
   ],
 })
 export class WorkflowModule {}
