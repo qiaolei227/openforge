ef23e0587c8d118d479a5b708c2d9e54d0639b6c
david.qiao <286887534@qq.com>
2026-05-18 02:23:10 +0800

    chore(server): wire AssigneeResolverService in WorkflowModule (P2.3)

--- apps/server/src/workflow/workflow.module.ts
+++ apps/server/src/workflow/workflow.module.ts
@@ -2,11 +2,22 @@ import { Module } from '@nestjs/common';
 import { WorkflowService } from './workflow.service';
 import { WorkflowVersionService } from './workflow-version.service';
 import { WorkflowConditionMatcher } from './workflow-condition-matcher.service';
+import { AssigneeResolverService } from './assignee-resolver.service';
 import { WorkflowController } from './workflow.controller';
 
 @Module({
   controllers: [WorkflowController],
-  providers: [WorkflowService, WorkflowVersionService, WorkflowConditionMatcher],
-  exports: [WorkflowService, WorkflowVersionService, WorkflowConditionMatcher],
+  providers: [
+    WorkflowService,
+    WorkflowVersionService,
+    WorkflowConditionMatcher,
+    AssigneeResolverService,
+  ],
+  exports: [
+    WorkflowService,
+    WorkflowVersionService,
+    WorkflowConditionMatcher,
+    AssigneeResolverService,
+  ],
 })
 export class WorkflowModule {}
