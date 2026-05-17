d3a8c203c072fc84882b04974084b89247693b1a
david.qiao <286887534@qq.com>
2026-05-18 02:05:26 +0800

    chore(server): register NotificationGateway in module (P2.3)

--- apps/server/src/notification/notification.module.ts
+++ apps/server/src/notification/notification.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
+import { NotificationService } from './notification.service';
+import { NotificationController } from './notification.controller';
+import { NotificationGateway } from './notification.gateway';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
-  exports: [],
+  imports: [AuthModule],
+  controllers: [NotificationController],
+  providers: [NotificationService, NotificationGateway],
+  exports: [NotificationService],
 })
 export class NotificationModule {}
